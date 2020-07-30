9cae92218892e604d1a5c08a61d1d79acde9da31
Alex Velasco <cristianvc-99@hotmail.com>
2020-07-30 18:39:57 -0500

    Initial Commit

---
+++ PlatziFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Alejandro Velasco on 30/07/20.
+//  Copyright © 2020 Alejandro Velasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
